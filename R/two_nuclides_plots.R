e69c2fa8b5fc62714f2ff48a457bb209eb17d52f
Vincent Godard <godard@cerege.fr>
2022-10-06 16:55:30 +0200

    Add function tnp_burial

--- R/two_nuclides_plots.R
+++ R/two_nuclides_plots.R
@@ -84,6 +84,89 @@ tnp_curves <- function(P1,P2,Lambda,S,rho,dlim=c(0.1,1000),alim=c(100,10e6),n=10
 }
 
 
-
+#' Compute burial age and denudation rate curves array for two-nuclides plots
+#'
+#'
+#'
+#' @param A burial age vector (a)
+#' @param E erosion rate vector (m/Ma)
+#' @param p1 production and decay parameters for the first nuclide (4 elements vector)
+#' \itemize{
+#'  \item p1[1] unscaled spallation production rate (at/g/a)
+#'  \item p1[2] unscaled stopped muons production rate (at/g/a)
+#'  \item p1[3] unscaled fast muons production rate (at/g/a)
+#'  \item p1[4] decay constant (1/a)
+#' }
+#' @param p2 production and decay parameters for the second nuclide (4 elements vector, same as p1)
+#' @param L Attenuation length (3 elements vector in g/cm2)
+#' \itemize{
+#' \item L[1]  neutrons
+#' \item L[2]  stopped muons
+#' \item L[3]  fast muons
+#' }
+#' @param S scaling factors (2 elements vector)
+#' \itemize{
+#' \item S[1]  scaling factor for spallation
+#' \item S[2]  scaling factor for muons
+#' }
+#' @param rho material density (g/cm3)
+#' @param n number of along-curve evaluation points (optional default 100)
+#'
+#' @return a list with two dataframes containing the concentrations for the two nuclides as a function of burial age and denudation
+#' @export
+#'
+#' @examples
+#' data("prm") # production and decay data
+#' p = prm
+#' data("Lambda") # attenuation length data
+#' L = Lambda
+#' altitude = 1000 # site elevation in m
+#' latitude = 20 # site latitude in degrees
+#' P = atm_pressure(alt=altitude,model="stone2000") # atmospheric pressure at site
+#' S = scaling_st(P,latitude) # Stone 2000 scaling parameters
+#' rho = 2.65 # bedrock density (g/cm3)
+#' N1 = "Be10" # longer half-life
+#'N2 = "Al26" # shorter half-life
+#'res = tnp_curves(prm[,N1],prm[,N2],Lambda,S,rho)
+#'plot(NA,xlim=c(0.25,10),ylim=c(0.5,7),log="x",
+#'     xlab=paste(N1,"(x10^6 at/g)"),ylab=paste(N2,"/",N1))
+#'lines(res[[1]]$C1/1e6,res[[1]]$C2/res[[1]]$C1,lty=2,lwd=2,col="khaki4") # constant exposure
+#'lines(res[[2]]$C1/1e6,res[[2]]$C2/res[[2]]$C1,lty=1,lwd=2,col="khaki4") # steady-state erosion
+#' A = c(1,2,3,4)*1e6 # increments in age (a)
+#' E = c(0.1,0.2,0.5,1,2,5,10) # increments in denudation rate (m/Ma)
+#' res = tnp_burial(A,E,prm[,N1],prm[,N2],L,S,rho,n=100) # compute array
+#' lines(res[[1]]$C1/1e6,res[[1]]$C2/res[[1]]$C1,col="grey") # constant age
+#' lines(res[[2]]$C1/1e6,res[[2]]$C2/res[[2]]$C1,col="black") # constant denudation
+tnp_burial <-function(A,E,p1,p2,L,S,rho,n=100){
+  p1 = as.numeric(p1)
+  p2 = as.numeric(p2)
+  L = as.numeric(L)
+  S = as.numeric(S)
+  A = as.numeric(A)
+  E = as.numeric(E)
+  # courbes iso age
+  for (i in 1:length(A)){
+    res = data.frame(A=rep(A[i],n),E=10^seq(log10(min(E)),log10(max(E)),length.out = n))
+    C1_0 = solv_conc_eul(0,res$E*rho*100/1e6,Inf,0,p1,S,Lambda)
+    res$C1 = solv_conc_eul(Inf,0,A[i],C1_0,p1,S,Lambda)
+    C2_0 = solv_conc_eul(0,res$E*rho*100/1e6,Inf,0,p2,S,Lambda)
+    res$C2 = solv_conc_eul(Inf,0,A[i],C2_0,p2,S,Lambda)
+    res[nrow(res)+1,]<-NA # for breaks in lines
+    if(i==1){res_A=res}else{res_A=rbind(res_A,res)}
+  }
+  # courbes iso erosion
+  for (i in 1:length(E)){
+    res = data.frame(A=seq(0,max(A),length.out = n),E=rep(E[i],n))
+    C1_0 = solv_conc_eul(0,E[i]*rho*100/1e6,Inf,0,p1,S,Lambda)
+    res$C1 = solv_conc_eul(Inf,0,res$A,C1_0,p1,S,Lambda)
+    C2_0 = solv_conc_eul(0,E[i]*rho*100/1e6,Inf,0,p2,S,Lambda)
+    res$C2 = solv_conc_eul(Inf,0,res$A,C2_0,p2,S,Lambda)
+    res[nrow(res)+1,]<-NA # for breaks in lines
+    if(i==1){res_E=res}else{res_E=rbind(res_E,res)}
+  }
+  res = list(res_A,res_E)
+  names(res)<-c("cst_age","cst_denudation")
+  return(res)
+}
 
 
