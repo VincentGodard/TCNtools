891d0d61b394a45d78442afa1fbce96c43dd2934
Vincent Godard <godard@cerege.fr>
2022-10-06 21:46:07 +0200

    remove shiny launcher

--- R/utils.R
+++ R/utils.R
@@ -229,15 +229,5 @@ rc_ndp <- function(time,lat,lon){
 
 
 
-#' @export
-sapp_launch<-function(app){
-  app_list = c("2_nuclides","production_rates","profile_modelling","sato_fluxes",
-               "simple_accumulation","transient_erosion")
-  if (!(app %in% app_list)) { stop(paste("app must be one of :",paste(app_list,collapse=" ")))}
-  appDir <- system.file(paste("shiny_apps/",app,sep=""),package="TCNtools")
-  if (appDir == "") {
-    stop("Could not find app directory. Try re-installing `TCNtools`.", call. = FALSE)
-  }
-  shiny::shinyAppDir(appDir,options = list(width = "100%", height = 700))
-}
+
 
